7c63285ad223f6c7ceff64c4d6e687ce13981de7
Artturi Kerminen <kerminenartturi@gmail.com>
2023-01-07 17:56:56 +0200

    Rename main file

---
+++ src/GameWrapper.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useMemo, useState } from "react";
+
+export const CharacterGuessWrapper = (props) => {
+  let [characterData, setCharacterData] = useState([]);
+  let [characterNames, setCharacterNames] = useState([]);
+
+  let [input, setInput] = useState("");
+
+  const infoSpanStyle = {
+    minWidth: "75px",
+    maxWidth: "150px",
+    textAlign: "center",
+  };
+
+  return (
+    <div className="w-full">
+      <h1
+        style={{ fontSize: "90px", textAlign: "center" }}
+        className="p-2 mb-52"
+      >
+        TESDLE
+      </h1>
+
+      <div className="mt-auto w-full p-2 flex flex-col drop-shadow-xl items-center">
+        {/*Info box*/}
+        <div className="flex flex-row p-2 text-lg w-1/2 bg-white rounded-lg shadow-md">
+          <span style={infoSpanStyle} className="w-1/6">
+            Set
+          </span>
+          <span style={infoSpanStyle} className="w-1/6">
+            Type
+          </span>
+          <span style={infoSpanStyle} className="w-1/6">
+            Subtype
+          </span>
+          <span style={infoSpanStyle} className="w-1/6">
+            Attributes
+          </span>
+          <span style={infoSpanStyle} className="w-1/6">
+            Class
+          </span>
+          <span style={infoSpanStyle} className="w-1/6">
+            Rarity
+          </span>
+        </div>
+        <div className="flex flex-col gap-y-2 w-1/2"></div>
+      </div>
+    </div>
+  );
+};
