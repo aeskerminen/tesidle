3f89371144dd5cee36903c6073399601149546c3
Artturi Kerminen <kerminenartturi@gmail.com>
2023-01-08 12:24:31 +0200

    UI changes

--- src/CardWrapper.js
+++ src/CardWrapper.js
@@ -2,9 +2,9 @@ import React from "react";
 
 export const CardWrapper = (props) => {
   return (
-    <div className="flex flex-row p-1 mt-2 bg-black shadow-xl rounded-lg align-middle">
+    <div className="grid grid-cols-6 p-1 mt-2 bg-black shadow-xl rounded-lg align-middle items-center">
       <span
-        className={`p-0.5 m-0.5 rounded-lg shadow-md w-1/6 text-center bg-white ${
+        className={`m-0.5 p-0.5 rounded-lg shadow-md text-center h-full bg-white ${
           props.card["Set"]
             ? props.card["Set"] === props.target["Set"]
               ? "bg-green-500"
@@ -15,7 +15,7 @@ export const CardWrapper = (props) => {
         {props.card["Set"] || "N/A"}
       </span>
       <span
-        className={`p-0.5 m-0.5 rounded-lg shadow-md w-1/6 text-center bg-white ${
+        className={`m-0.5 p-0.5 rounded-lg shadow-md text-center h-full bg-white ${
           props.card["Type"]
             ? props.card["Type"] === props.target["Type"]
               ? "bg-green-500"
@@ -26,7 +26,7 @@ export const CardWrapper = (props) => {
         {props.card["Type"] || "N/A"}
       </span>
       <span
-        className={`p-0.5 m-0.5 rounded-lg shadow-md w-1/6 text-center bg-white ${
+        className={`m-0.5 p-0.5 rounded-lg shadow-md text-center h-full bg-white ${
           props.card["Subtype"]
             ? props.card["Subtype"] === props.target["Subtype"]
               ? "bg-green-500"
@@ -37,7 +37,7 @@ export const CardWrapper = (props) => {
         {props.card["Subtype"] || "N/A"}
       </span>
       <span
-        className={`p-0.5 m-0.5 rounded-lg shadow-md w-1/6 text-center bg-white ${
+        className={`m-0.5 p-0.5 rounded-lg shadow-md text-center h-full bg-white ${
           props.card["Attribute(s)"]
             ? props.card["Attribute(s)"] === props.target["Attribute(s)"]
               ? "bg-green-500"
@@ -48,7 +48,7 @@ export const CardWrapper = (props) => {
         {props.card["Attribute(s)"] || "N/A"}
       </span>
       <span
-        className={`p-0.5 m-0.5 rounded-lg shadow-md w-1/6 text-center bg-white ${
+        className={`m-0.5 p-0.5 rounded-lg shadow-md text-center h-full bg-white ${
           props.card["Class"]
             ? props.card["Class"] === props.target["Class"]
               ? "bg-green-500"
@@ -59,7 +59,7 @@ export const CardWrapper = (props) => {
         {props.card["Class"] || "N/A"}
       </span>
       <span
-        className={`p-0.5 m-0.5 rounded-lg shadow-md w-1/6 text-center bg-white ${
+        className={`m-0.5 p-0.5 rounded-lg shadow-md text-center h-full bg-white ${
           props.card["Rarity"]
             ? props.card["Rarity"] === props.target["Rarity"]
               ? "bg-green-500"
--- src/GameWrapper.js
+++ src/GameWrapper.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useMemo, useState } from "react";
 
+import background from "./img/sk_bg.jpg";
+
 import Cards from "./data/cards.json";
 
 import { CardWrapper } from "./CardWrapper";
@@ -14,6 +16,8 @@ export const GameWrapper = (props) => {
 
   let [input, setInput] = useState("");
 
+  let [showHelp, setShowHelp] = useState(false);
+
   const infoSpanStyle = {
     minWidth: "75px",
     maxWidth: "150px",
@@ -25,6 +29,8 @@ export const GameWrapper = (props) => {
     setCardData((cardData = [...values]));
 
     getTargetCard();
+
+    console.log(targetCard);
   }, []);
 
   function getTargetCard() {
@@ -33,60 +39,143 @@ export const GameWrapper = (props) => {
   }
 
   return (
-    <div className="w-full">
-      <h1
-        style={{ fontSize: "90px", textAlign: "center" }}
-        className="p-2 mb-52"
+    <div className="h-full">
+      {/* Card help */}
+      {showHelp && (
+        <div
+          className="bg-white rounded-lg shadow-md p-2 absolute overflow-y-scroll"
+          style={{
+            height: "75%",
+            width: "75%",
+            zIndex: 999,
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0,
+            margin: "auto",
+          }}
+        >
+          <div className="grid grid-cols-4">
+            {cardData.map((card) => {
+              return (
+                <div className="m-1 p-1 flex flex-col items-center bg-gray-400 shadow border-2 border-black rounded-sm">
+                  <span className="text-2xl p-1">{card["Name"]}</span>
+                  <img
+                    className="p-1 m-1 shadow-md bg-white"
+                    src={`${process.env.PUBLIC_URL}/cards/${card["Name"]}.jpg`}
+                  ></img>
+                  <div className="p-1 m-1 mb-2 bg-white flex flex-col gap-y-1 w-full">
+                    <div className="flex flex-row gap-x-2">
+                      <span className="p-2 bg-slate-200">Set:</span>
+                      <span className="p-2 bg-slate-200 grow text-center">
+                        {card["Set"]}
+                      </span>
+                    </div>
+                    <div className="flex flex-row gap-x-2">
+                      <span className="p-2 bg-slate-200">Type:</span>
+                      <span className="p-2 bg-slate-200 grow text-center">
+                        {card["Type"]}
+                      </span>
+                    </div>
+                    <div className="flex flex-row gap-x-2">
+                      <span className="p-2 bg-slate-200">Subtype:</span>
+                      <span className="p-2 bg-slate-200 grow text-center">
+                        {card["Subtype"]}
+                      </span>
+                    </div>
+                    <div className="flex flex-row gap-x-2">
+                      <span className="p-2 bg-slate-200">Attribute(s):</span>
+                      <span className="p-2 bg-slate-200 grow text-center">
+                        {card["Attribute(s)"]}
+                      </span>
+                    </div>
+                    <div className="flex flex-row gap-x-2">
+                      <span className="p-2 bg-slate-200">Rarity:</span>
+                      <span className="p-2 bg-slate-200 grow text-center">
+                        {card["Rarity"]}
+                      </span>
+                    </div>
+                    <div className="flex flex-row gap-x-2">
+                      <span className="p-2 bg-slate-200">Effects:</span>
+                      <span className="p-2 bg-slate-200 grow text-center">
+                        {card["Effects"]}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      )}
+      <div
+        className={`w-full h-full ${showHelp ? "blur-sm" : "blur-none"}`}
+        style={{ backgroundImage: `url(${background})` }}
       >
-        TESDLE
-      </h1>
+        <h1
+          style={{ fontSize: "90px", textAlign: "center" }}
+          className="p-2 mb-52"
+        >
+          TESDLE
+        </h1>
 
-      <div className="mt-auto w-full p-2 flex flex-col drop-shadow-xl items-center">
-        {/* Card input */}
-        <div className="mb-2">
-          <input
-            onChange={(e) => {
-              setInput(e.target.value);
-            }}
-            placeholder="Name a Card..."
-            className="p-2 shadow-md focus:outline-none"
-          ></input>
-          <button
-            onClick={() => {
-              setPlayedCards((playedCards = [...playedCards, Cards[input]]));
-            }}
-            type="button"
-            className="p-2 shadow-md bg-white ml-1"
-          >
-            Lay
-          </button>
-        </div>
+        <button
+          type="button"
+          className="bg-white shadow-md rounded p-2 text-center text-xl absolute z-50 mt-16 -ml-2"
+          onClick={() => {
+            setShowHelp(!showHelp);
+          }}
+        >
+          {">"}
+        </button>
 
-        {/* Info box */}
-        <div className="flex flex-row p-2 text-lg w-1/2 bg-white rounded-lg shadow-md">
-          <span style={infoSpanStyle} className="w-1/6">
-            Set
-          </span>
-          <span style={infoSpanStyle} className="w-1/6">
-            Type
-          </span>
-          <span style={infoSpanStyle} className="w-1/6">
-            Subtype
-          </span>
-          <span style={infoSpanStyle} className="w-1/6">
-            Attributes
-          </span>
-          <span style={infoSpanStyle} className="w-1/6">
-            Class
-          </span>
-          <span style={infoSpanStyle} className="w-1/6">
-            Rarity
-          </span>
-        </div>
-        <div className="flex flex-col gap-y-2 w-1/2">
-          {playedCards.map((cur) => {
-            return <CardWrapper card={cur} target={targetCard}></CardWrapper>;
-          })}
+        <div className="mt-auto w-full p-2 flex flex-col drop-shadow-xl items-center">
+          {/* Card input */}
+          <div className="mb-2">
+            <input
+              onChange={(e) => {
+                setInput(e.target.value);
+              }}
+              placeholder="Name a Card..."
+              className="p-2 shadow-md focus:outline-none"
+            ></input>
+            <button
+              onClick={() => {
+                setPlayedCards((playedCards = [Cards[input], ...playedCards]));
+              }}
+              type="button"
+              className="p-2 shadow-md bg-white ml-1"
+            >
+              Lay
+            </button>
+          </div>
+
+          {/* Info box */}
+          <div className="grid grid-cols-6 p-2 bg-white rounded-lg shadow-md w-2/3 justify-items-center">
+            <span style={infoSpanStyle} className="w-1/6">
+              Set
+            </span>
+            <span style={infoSpanStyle} className="w-1/6">
+              Type
+            </span>
+            <span style={infoSpanStyle} className="w-1/6">
+              Subtype
+            </span>
+            <span style={infoSpanStyle} className="w-1/6">
+              Attributes
+            </span>
+            <span style={infoSpanStyle} className="w-1/6">
+              Class
+            </span>
+            <span style={infoSpanStyle} className="w-1/6">
+              Rarity
+            </span>
+          </div>
+          <div className="flex flex-col gap-y-2 w-2/3">
+            {playedCards.map((cur) => {
+              return <CardWrapper card={cur} target={targetCard}></CardWrapper>;
+            })}
+          </div>
         </div>
       </div>
     </div>
