34c5b7aede5a6b4cffc83033424c3828351d770d
Artturi Kerminen <kerminenartturi@gmail.com>
2023-01-08 12:24:50 +0200

    Remove background from root

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { useState } from "react";
 import { GameWrapper } from "./GameWrapper";
 import HomePage from "./HomePage";
-import background from "./img/sk_bg.jpg";
 
 let test = true;
 
 const App = () => {
   return (
-    <div style={{ backgroundImage: `url(${background})`, height: "100vh" }}>
+    <div style={{ height: "100vh" }}>
       {!test && <HomePage></HomePage>}
       {test && <GameWrapper></GameWrapper>}
     </div>
